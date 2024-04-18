4f0baa30896a3be5b6d2d83be15dec3b14dfd483
Chad Wright <justdeejays@gmail.com>
2024-04-18 16:08:13 +0200

    feat: add fonts, images & custom tailwind classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,7 @@
-"use client";
-
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
-
-import { showEmail } from "@/app/_actions/actions";
-
 export default function Home() {
-  const [displayEmail, setDisplayEmail] = useState("Show email address");
-
-  const handleShowEmail = async () => {
-    const email = await showEmail();
-    setDisplayEmail(email);
-  };
-
   return (
-    <main className="grid place-items-center h-screen bg-[#2D2C33] px-10">
-      <div className="grid gap-10 justify-items-center max-w-[700px]">
-        <h1 className="text-4xl text-center lg:text-7xl text-[#A77655]">
-          Something new and exciting is coming soon...
-        </h1>
-        <Image
-          src="/monks-medical-logo-1.svg"
-          alt="Monk's Medical Logo"
-          width={400}
-          height={400}
-          className="image-pulse py-8 border-y-2 border-white"
-        />
-        <ul className="grid gap-4 justify-items-center lg:gap-1">
-          <li>
-            {displayEmail === "Show email address" ? (
-              <span
-                className="text-[#DDC3A0] hover:text-[#DDC3A0]/80 p-2 -m-2 lg:p-0 lg:m-0 cursor-pointer italic"
-                onClick={handleShowEmail}
-              >
-                {displayEmail}
-              </span>
-            ) : (
-              <Link
-                href="mailto:monksmedical@gmail.com"
-                className="text-[#DDC3A0] hover:text-[#DDC3A0]/80 p-2 -m-2 lg:p-0 lg:m-0"
-              >
-                monksmedical@gmail.com
-              </Link>
-            )}
-          </li>
-          <li>
-            <Link
-              href="tel:+27440200431"
-              className="text-[#DDC3A0] hover:text-[#DDC3A0]/80 p-2 -m-2 lg:p-0 lg:m-0"
-            >
-              +27 44 020 0431
-            </Link>
-          </li>
-        </ul>
-      </div>
+    <main>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, nostrum!
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,71 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontSize: {
+      heading: [
+        "3rem",
+        {
+          lineHeight: "110%",
+          letterSpacing: "6%",
+          fontWeight: "700",
+        },
+      ],
+      subheading: [
+        "1.5rem",
+        {
+          lineHeight: "120%",
+          letterSpacing: "4%",
+          fontWeight: "500",
+        },
+      ],
+      paragraph: [
+        "1rem",
+        {
+          lineHeight: "125%",
+          letterSpacing: "2%",
+          fontWeight: "300",
+        },
+      ],
+      button: [
+        "1rem",
+        {
+          lineHeight: "125%",
+          letterSpacing: "2%",
+          fontWeight: "500",
+        },
+      ],
+      logo: [
+        "1rem",
+        {
+          lineHeight: "90%",
+          letterSpacing: "5.5%",
+          fontWeight: "500",
+        },
+      ],
     },
+    fontFamily: {
+      larsseit: ["Larsseit"],
+    },
+    screens: {
+      phone: "400px",
+      tablet: "600px",
+      tabletLarge: "950px",
+      desktopSmall: "1100px",
+      desktop: "1450px",
+    },
+    colors: {
+      white: "#ffffff",
+      black: "#2D2C33",
+      brown: "#A77655",
+      lightBrown: "#DDC3A0",
+      khaki: "#A5996D",
+      green: "#A6B2A9",
+      link: "#0000EE",
+    },
+    dropShadow: {
+      default: "0 4px 4px rgba(45, 44, 51, 0.25)",
+    },
+    extend: {},
   },
   plugins: [],
 };
